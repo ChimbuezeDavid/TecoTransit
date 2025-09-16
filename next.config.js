084f49ce409347044f40a6386ed349fa95e6924d
Chimbueze David <chimdaveo@gmail.com>
2025-09-16 09:24:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('@ducanh2912/next-pwa')({
+const pwa = require('@ducanh2912/next-pwa');
+const withPWA = pwa.default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
