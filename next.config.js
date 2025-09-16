7f1e0bc5c634d398309f751ff1e9da42730e9468
Chimbueze David <chimdaveo@gmail.com>
2025-09-16 08:33:05 +0000

    Here is a copy of my browser log:
    logger.js:46 workbox Router is respon

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   /* config options here */
   typescript: {
@@ -37,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
