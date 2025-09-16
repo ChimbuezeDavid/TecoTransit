faef0ee2e229e0eb0611ba49229df4f27fcacdad
Chimbueze David <chimdaveo@gmail.com>
2025-09-16 08:25:51 +0000

    Manifest: property 'start_url' ignored, URL is invalid.Understand this w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3e%3ctext x='50' y='50' text-anchor='middle' dominant-baseline='middle' font-size='55' font-weight='bold' fill='yellow'%3eTeco%3c/text%3e%3c/svg%3e" />
-        <link rel="manifest" href="data:application/json;base64,ew0KICAic2hvcnRfbmFtZSI6ICJUZWNvVHJhbnNpdCIsDQogICJuYW1lIjogIlRlY29UcmFuc2l0IiwNCiAgImRlc2NyaXB0aW9uIjogIkJvb2sgWW91ciBUcmlwIHdpdGggVGVjb1RyYW5zaXQuIEZhc3QsIHJlbGlhYmxlLCBhbmQgY29tZm9ydGFibGUgcmlkZXMgdG8geW91ciBkZXN0aW5hdGlvbi4iLA0KICAiaWNvbnMiOiBbDQogICAgew0KICAgICAgInNyYyI6ICIvZmF2aWNvbi5pY28iLA0KICAgICAgInNpemVzIjogIjY0eDY0IDMyeDMyIDI0eDI0IDE2eDE2IiwNCiAgICAgICJ0eXBlIjogImltYWdlL3gtbGNvbiINCiAgICB9LA0KICAgIHsNCiAgICAgICJzcmMiOiAiL2ljb24tMTkyeDE5Mi5wbmciLA0KICAgICAgInR5cGUiOiAiaW1hZ2UvcG5nIiwNCiAgICAgICJzaXplcyI6ICIxOTJ4MTkyIg0KICAgIH0sDQogICAgew0KICAgICAgInNyYyI6ICIvaWNvbi01MTJ4NTEyLnBuZyIsDQogICAgICJ0eXBlIjogImltYWdlL3BuZyIsDQogICAgICAic2l6ZXMiOiAiNTEyeDUxMiINCiAgICB9DQogIF0sDQogICJzdGFydF91cmwiOiAiLyIsDQogICJkaXNwbGF5IjogInN0YW5kYWxvbmUiLA0KICAidGhlbWVfY29sb3IiOiAiIzAwMDAwMCIsDQogICJiYWNrZ3JvdW5kX2NvbG9yIjogIiMwMDAwMDAiDQp9" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         <meta name="theme-color" content="#fff" />
         <title>TecoTransit</title>
